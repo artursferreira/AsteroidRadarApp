197bad730594c32b8b80bd9a07c2eb0283da45cd
Artur <artursferreira27@gmail.com>
2021-01-28 22:37:39 -0300

    Update Constants.kt

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val NASA_KEY = "2SxG7wHWAujovIy8P1NMxFMZeZDcoo6CgaKABwVw"
+    const val NASA_KEY = ""
     const val MEDIA_TYPE_IMAGE = "image"
 }
